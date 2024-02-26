adce41dbbf661117917dfd71e7049f49efd37cf7
DustWolverine <akashrajnitrr@gmail.com>
2024-02-26 17:30:34 +0530

    stack and heap memory

--- datatyperevision.js
+++ datatyperevision.js
@@ -10,3 +10,43 @@ let bigNumber=123n;
 const id=Symbol("123");
 
 console.table([typeof number,typeof isLoggedIn,typeof city,typeof country,typeof bigNumber,typeof id]);
+
+
+//?Reference (Non primitive)
+//! Array,object,function
+
+const heros=["Spiderman", "Batman","Aquaman","Superman"];
+
+let user={
+    name:"Akash",
+    age:25,
+    city:"Bangalore"
+}
+console.log(user);
+console.table([user.name,user.age,user.city]);
+
+
+const myFunction=function(){
+console.log("Hello world!");
+}
+
+
+
+// * stack(primative) heap(Non primitive)
+
+
+let myname="Akashraj"
+let myname2=myname;
+myname2="Rajakash";
+console.log(myname2);
+console.log(myname);
+
+let userone={
+    email:"akashrajnitrr@gmail.com",
+    upi:"rajakash@okaxis"
+}
+let usertwo=userone;
+
+console.table([userone]);
+usertwo.email="rajakash7001@gmail.com";
+console.table([userone]);
