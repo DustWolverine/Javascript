c410cf5aa54df9fed72729c9eec74c9e7bbcaff2
DustWolverine <akashrajnitrr@gmail.com>
2024-02-26 17:00:30 +0530

    Datatype revision

---
+++ datatyperevision.js
@@ -0,0 +1,12 @@
+//? Premetive datatypes
+//! Bigint,number,string,boolean,null,undefined,Symbol
+
+const number=123;
+let Name="Akash";
+let isLoggedIn=false;
+let city;
+let country=null;
+let bigNumber=123n;
+const id=Symbol("123");
+
+console.table([typeof number,typeof isLoggedIn,typeof city,typeof country,typeof bigNumber,typeof id]);
